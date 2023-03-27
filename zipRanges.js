a036339c6bffcf0a80dad2503f1e3de9918f1694
Gavin Butler <GavinButler_33@msn.com>
2023-03-27 12:47:14 -0400

    Final version

--- zipRanges.js
+++ zipRanges.js
@@ -3,13 +3,25 @@ const zipRanges = (rangeStart, rangeEnd) => {
   for (let i = 0; i < 67; i++) {
     const end = rangeEnd[i];
     const start = rangeStart[i];
-    const section = []
+    const section = [];
     for (let j = 0; j < end.length; j++) {
       section.push([start[j], end[j]]);
     }
-    ranges.push(section)
+    ranges.push(section);
   }
-  return ranges
+  function downloadObjectAsJson(exportObj, exportName) {
+    var dataStr =
+      'data:text/json;charset=utf-8,' +
+      encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute('href', dataStr);
+    downloadAnchorNode.setAttribute('download', exportName + '.json');
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+  downloadObjectAsJson(ranges[66], 'ranges');
+  return ranges;
 };
 
-export default zipRanges
+export default zipRanges;
